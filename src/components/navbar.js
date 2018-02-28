6fc90b593858b94750c2e29a58d359baa8bb516e
Tabebee <tabibone@gmail.com>
2018-02-28 00:49:02 -0500

    changed navbar bar to class component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navmesaage from './nav-message-text'
 
-const Navbar = () => {
-    return (
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            score: 0,
+            highscore: 0
+        };
+    }
+
+    render() {
+        return (
             <nav className='navbar navbar-dark bg-dark navbar-center'>
                 <div> </div>  {/* This is purposely done to center the next content */}
                 <div> nav </div>
                 <Navmesaage className='right'/>
             </nav>
-    )
+        )
+    }
 }
+
+
+
+
+// const Navbar = () => {
+//     return (
+//             <nav className='navbar navbar-dark bg-dark navbar-center'>
+//                 <div> </div>  {/* This is purposely done to center the next content */}
+//                 <div> nav </div>
+//                 <Navmesaage className='right'/>
+//             </nav>
+//     )
+// }
 
 export default Navbar;
